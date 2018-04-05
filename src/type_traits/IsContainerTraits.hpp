e051cd17aa1830e4e47b01bcce361b51fb148e6e
siskinc <susecjh@gmail.com>
2018-04-05 15:55:30 +0800

    add container traits

--- src/type_traits/IsContainerTraits.hpp
+++ src/type_traits/IsContainerTraits.hpp
@@ -15,48 +15,48 @@
 #include <unordered_map>
 #include <type_traits>
 
-template <typename T> struct container_traits {
+template <typename T> struct is_container_traits {
     static bool const value = false;
 };
 
 template <typename... Args>
-struct container_traits<std::vector<Args...>> {
+struct is_container_traits<std::vector<Args...>> {
 static bool const value = true;
 };
 
 template <typename... Args>
-struct container_traits<std::deque<Args...>> {
+struct is_container_traits<std::deque<Args...>> {
 static bool const value = true;
 };
 
 template <typename... Args>
-struct container_traits<std::list<Args...>> {
+struct is_container_traits<std::list<Args...>> {
 static bool const value = true;
 };
 
 template <typename... Args>
-struct container_traits<std::set<Args...>> {
+struct is_container_traits<std::set<Args...>> {
 static bool const value = true;
 };
 
 template <typename... Args>
-struct container_traits<std::map<Args...>> {
+struct is_container_traits<std::map<Args...>> {
 static bool const value = true;
 };
 
 template <typename... Args>
-struct container_traits<std::unordered_set<Args...>> {
+struct is_container_traits<std::unordered_set<Args...>> {
 static bool const value = true;
 };
 
 template <typename... Args>
-struct container_traits<std::unordered_map<Args...>> {
+struct is_container_traits<std::unordered_map<Args...>> {
 static bool const value = true;
 };
 
 template<typename T>
 struct is_std {
-    static constexpr bool const value = container_traits<T>::value;
+    static constexpr bool const value = is_container_traits<T>::value;
 };
 
 
